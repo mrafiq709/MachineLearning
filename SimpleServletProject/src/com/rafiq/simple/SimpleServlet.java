7bbf4198144e5f3903c11d50de04eda0696165db
RAFIQ <mrafiq709@gmail.com>
2018-04-16 22:29:08 +0600

    Initial Commit

---
+++ SimpleServletProject/src/com/rafiq/simple/SimpleServlet.java
@@ -0,0 +1,32 @@
+package com.rafiq.simple;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class SimpleServlet
+ */
+@WebServlet(description = "A simple servlet", urlPatterns = { "/SimpleServletPath" })
+public class SimpleServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		System.out.println("Hello from get method.");
+		PrintWriter writer = response.getWriter();
+
+		writer.println("<html>" + "<h3>Hello In HTML</h3>" + "</html>");
+	}
+
+}
